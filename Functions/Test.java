95d74edfdba78b154090b033a8e405fe87eef110
Chhatish kumar <127961547+ChhatishK@users.noreply.github.com>
2024-07-10 09:36:20 +0000

    Finding smallest amonst three

--- Functions/Test.java
+++ Functions/Test.java
@@ -4,6 +4,10 @@ class Addition {
     int sum(int a, int b) {
         return a + b;
     }
+
+    double sum(double a, double b) {
+        return a + b;
+    }
 }
 
 public class Test {
@@ -14,15 +18,30 @@ public class Test {
         return a + b;
     }
 
+    // Write a java method to find the smallest number among three numbers
+    private static int smallesAmongstThree(int a, int b , int c) {
+        if (a < b && a < c) {
+            return a;
+        } else if (b < a && b < c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
     void hello() {
         System.out.println("Hello World!");
     }
     public static void main(String[] args) {
-        System.out.println(add(2,3));
-        Test t = new Test();
-        t.hello();
-        Addition a = new Addition();
-        System.out.println(a.sum(2, 5));
+        // System.out.println(add(2,3));
+        // Test t = new Test();
+        // t.hello();
+        // Addition a = new Addition();
+        // System.out.println(a.sum(2, 5));
+        // System.out.println(a.sum(2.0, 5.0));
+
+        int a = 3, b = 4, c = 2;
+        System.out.println(smallesAmongstThree(a, b, c));
     }
     
 }
