0f1ba5a44dbe378183bee766d92b66c1447cb093
Chhatish kumar <127961547+ChhatishK@users.noreply.github.com>
2024-07-11 05:40:11 +0000

    sum of digit

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class Fibonacci {
+    static int fib(int n) {
+        if (n==0 || n==1) {
+            return n;
+        }
+
+       else {
+            return fib(n-1)+fib(n-2);
+       }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i=0; i<n; i++) {
+            System.out.print(fib(i)+" ");
+        }
+        System.out.println();
+    }
+}
---
+++ Recursion/SumOfDigit.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class SumOfDigit {
+    static int sumOfDigit(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        
+        return  n%10 + sumOfDigit(n/10);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfDigit(234));
+    }
+}
