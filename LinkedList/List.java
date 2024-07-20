6c31851e5f2a332a851e525e82d3bed4b05c542c
Chhatish kumar <127961547+ChhatishK@users.noreply.github.com>
2024-07-20 10:00:53 +0000

    Adding node at last

--- LinkedList/List.java
+++ LinkedList/List.java
@@ -1,7 +1,12 @@
-import java.util.LinkedList;
+
 import java.util.Scanner;
 public class List {
     static Node head;
+    static int size;
+
+    public List() {
+        size = 0;
+    }
     public static class Node {
         int data;
         Node next;
@@ -9,6 +14,7 @@ public class List {
         Node(int data) {
             this.data = data;
             this.next = null;
+            size++;
         }
     }
 
@@ -34,7 +40,64 @@ public class List {
 
     }
 
-    static void displayList(Node head) {
+    static void insertAtPos(int val, int index) {
+        Node newNode = new Node(val);
+        if (index <= 0 || index >= size) {
+            System.out.println("Invalid Index!");
+            return;
+        }
+        if (index == 1 || head == null) {
+            insertAtBeg(val);
+            return;
+        }
+
+        Node nextNode = head.next;
+        Node prev = head;
+
+        for (int i=1; i<index-1; i++) {
+            nextNode = nextNode.next;
+            prev = prev.next;
+        }
+
+        newNode.next = nextNode;
+        prev.next = newNode;
+    }
+
+    static void deleteAtBeg() {
+        if (head == null) {
+            return;
+        }
+
+        head = head.next;
+        size--;
+        displayList();
+    }
+
+    static void deleteAtEnd() {
+        if (head == null) {
+            displayList();
+            return;
+        }
+
+        Node nextNode = head.next;
+        Node prev = head;
+
+        if (nextNode == null) {
+            deleteAtBeg();
+            size--;
+            return;
+        }
+        while (nextNode.next != null) {
+            nextNode = nextNode.next;
+            prev = prev.next;
+        }
+
+        prev.next = null;
+        size--;
+        displayList();
+    }
+
+    static void displayList() {
         if (head == null) {
             System.out.println("List is empty!");
             return;
@@ -45,15 +108,14 @@ public class List {
             System.out.print(temp.data+"->");
             temp = temp.next;
         }
-        System.out.println("Null");
+        System.out.print("Null");
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        List list = new List();
 
         while (true) {
-            System.out.println("\n1.insertAtBeg\n2.insertAtEnd\n3.displayList\nEnter choise : ");
+            System.out.println("\n1.insertAtBeg\n2.insertAtEnd\n3.displayList\n4.Delete at Begining\n5. Delete at End\n6.Insert at Index\nEnter choise : ");
             int choise = scan.nextInt();
             if (choise == 1) {
                 System.out.println("Enter a number into the list: ");
@@ -64,11 +126,24 @@ public class List {
                 int num = scan.nextInt();
                 insertAtEnd(num);
             } else if (choise == 3) {
-                displayList(head);
-            } else {
-                System.out.println("Program terminated!");
+                displayList();
+            } else if(choise == 4) {
+                deleteAtBeg();
+            } else if(choise == 5) {
+                deleteAtEnd();
+            } else if (choise == 6) {
+                System.out.println("Enter a number into the list : ");
+                int num = scan.nextInt();
+                System.out.println("Enter the position : ");
+                int pos = scan.nextInt();
+                insertAtPos(num, pos);
+            }
+            else {
+                System.out.println("Entered Wrong choise! \nProgram terminated!");
                 break;
             }
         }
+
+        scan.close();
     }
 }
