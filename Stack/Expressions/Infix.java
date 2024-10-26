fb32b4b1c6d2f490a631042855af5613e20c116d
Chhatish kumar <127961547+ChhatishK@users.noreply.github.com>
2024-10-26 19:12:36 +0000

    Infix Expression

---
+++ Stack/Expressions/Infix.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class Infix {
+
+    public static int infixEval(String str) {
+        Stack<Integer> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for (int i=0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+            int ascii = (int)ch;
+
+            if (ascii >= 48 && ascii <= 57) {
+                val.push(ascii - '0');
+            } else if (op.size() == 0 || ch == '(' || op.peek() == '(') {
+                op.push(ch);
+            } else if (ch == ')') {
+                while (op.peek() != '(') {
+                    int v2 = val.pop();
+                    int v1 = val.pop();
+                    char c = op.pop();
+
+                    if (c == '+') val.push(v1 + v2);
+                    else if (c == '-') val.push(v1 - v2);
+                    else if (c == '*') val.push(v1 * v2);
+                    else if (c == '/') val.push(v1 / v2);
+                }
+                op.pop();
+            } 
+            else {
+                if (ch == '+' || ch == '-') {
+                    int v2 = val.pop();
+                    int v1 = val.pop();
+                    char c = op.pop();
+                    if (c == '+') val.push(v1 + v2);
+                    else if (c == '-') val.push(v1 - v2);
+                    else if (c == '*') val.push(v1 * v2);
+                    else if (c == '/') val.push(v1 / v2);
+
+                    op.push(ch);
+                } else {
+                    if (op.peek() == '+' || op.peek() == '-') {
+                        op.push(ch);
+                    } else {
+                        int v2 = val.pop();
+                        int v1 = val.pop();
+                        char c = op.pop();
+
+                        if (c == '*') val.push(v1 * v2);
+                        else val.push(v1 / v2);
+
+                        op.push(ch);
+                    }
+                }
+            }
+
+            
+        }
+
+        while (val.size() > 1) {
+            int v2 = val.pop();
+            int v1 = val.pop();
+
+            char c = op.pop();
+
+            if (c == '+') val.push(v1 + v2);
+            if (c == '-') val.push(v1 - v2);
+            if (c == '*') val.push(v1 * v2);
+            if (c == '/') val.push(v1 / v2);
+        }
+
+        return val.pop();
+    }
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        String exp = scan.next();
+        System.out.println(infixEval(exp));
+    }
+}
