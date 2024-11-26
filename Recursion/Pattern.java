f52a84d5d9882220b90587c060597bb2103c0272
Chhatish kumar <127961547+ChhatishK@users.noreply.github.com>
2024-11-26 19:36:38 +0000

    Pattern

--- Recursion/Pattern.java
+++ Recursion/Pattern.java
@@ -1,11 +1,16 @@
 public class Pattern {
 
-    public static void reverseTriangle(int n) {
+    public static void reverseTriangle(int n, int c) {
         if (n == 0) return;
-        for (int i=0; i<n; i++) System.out.print("* ");
-        System.out.println();
 
-        reverseTriangle(n-1);
+        if (c < n) {
+            System.out.print("* ");
+            reverseTriangle(n, c+1);
+        } else {
+            System.out.println();
+            reverseTriangle(n-1, 0);
+        }
+        
     }
 
     public static void triangle(int n) {
@@ -13,16 +18,21 @@ public class Pattern {
     }
 
     public static void helper(int n, int c) {
-        if (c == n) return;
+        if (n == 0) return;
 
-        for (int i=0; i<=c; i++) System.out.print("* ");
-        System.out.println();
-
-        helper(n, c+1);
+        if (c < n) {
+            helper(n, c+1);
+            System.out.print("* ");
+        } else {
+            helper(n-1, 0);
+            System.out.println();
+        }
+        
     }
 
     public static void main(String args[]) {
         int n = 5;
-        triangle(n);
+        // triangle(n);
+        reverseTriangle(n, 0);
     }
 }
